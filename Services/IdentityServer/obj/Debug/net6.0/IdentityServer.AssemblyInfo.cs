6ca3917708166479977a7fd1c3ab75de96ece92e
ademceper <ceperadem@outlook.com>
2025-01-17 14:34:37 +0300

    Add Cargo Service

--- Services/IdentityServer/obj/Debug/net6.0/IdentityServer.AssemblyInfo.cs
+++ Services/IdentityServer/obj/Debug/net6.0/IdentityServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf32f1a72032f39501be6e983181b68e6f951c39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+010ce8ddbc6258cd93eca40fcb1bea0e30e7f3fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
